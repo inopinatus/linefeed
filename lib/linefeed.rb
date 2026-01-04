a92d6bee9c3152d94b9d72428e513585267d3dbe
Josh Goodall <inopinatus@hey.com>
2026-01-04 15:50:44 +1100

    better errors

--- lib/linefeed.rb
+++ lib/linefeed.rb
@@ -8,9 +8,17 @@ require "linefeed/version"
 # See README for more.
 
 module Linefeed
+  # Base error for linefeed-specific failures.
   class Error < StandardError; end
+  # Raised when linefeed is started more than once.
   class StartError < Error; end
-  class ClosedError < Error; end
+  # Raised when operations are attempted after close.
+  class ClosedError < Error
+    def initialize(message = "already closed")
+      super
+    end
+  end
+  # Raised when no handler is provided for line processing.
   class MissingHandler < ArgumentError; end
 
   # Set up linefeed processing and install a default handler.
